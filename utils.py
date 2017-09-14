45170099a913bd52b94334f0e158514eadc2328b
frontmaker <Djeka345#gmail.com>
2017-09-15 00:45:59 +0400

    create utlis module

---
+++ utils.py
@@ -0,0 +1,20 @@
+from telebot import types
+import config
+
+
+
+def generate_markup(right_answer, wrong_answers):
+    markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
+    all_answers = '{},{}'.format(right_answer, wrong_answers)
+    list_items = []
+
+    for item in all_answers.split(','):
+        list_items.append(item)
+
+    shuffle(list_items)
+
+    for item in list_items:
+        markup.add(item)
+        
+    return markup
+
